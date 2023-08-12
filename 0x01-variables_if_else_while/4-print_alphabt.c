27bebc46bd463f865a6bc02e6b1a91d1a0285abd
khaleelekan <kalilulekan@gmail.com>
2023-08-12 14:10:48 -0700

    changes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,21 @@
  * main- a program that prints alphabets,
  * except q and e
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch = 'a';
 
-	while (ch != 'e' && ch != 'q')
+	while (ch <= 'z')
+	{
+	if (ch != 'q' && ch != 'e')
 	{
 		putchar(ch);
 	}
 	ch++;
+	}
+	putchar('\n');
+
 	return (0);
 }
