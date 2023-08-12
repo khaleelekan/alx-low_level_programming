0195ef0525250e997543048b75554e014260fef2
khaleelekan <kalilulekan@gmail.com>
2023-08-12 14:27:49 -0700

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- a program that prints base ten digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+	printf("%d\n", i);
+	}
+	return (0);
+}
