f850bac6660d4c97e5b8ba6ebd71fe2bdcc7a89d
khaleelekan <kalilulekan@gmail.com>
2023-08-12 03:51:57 -0700

    task-4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- a program that prints alphabets,
+ * except q and e
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch != 'e' && ch != 'q')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
